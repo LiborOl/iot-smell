f149c0dc1b8b8edc1e200447fd5e3fea2c0afde7
Ales Novy <ales.novy@hp.com>
2016-11-25 14:25:17 +0100

    AQ Detectors API

--- routes/api.js
+++ routes/api.js
@@ -1,4 +1,6 @@
 var express = require('express');
+var fs = require("fs");
+var path = require('path');
 
 var settingsData = require('../services/settings.js');
 var sensors = require('../services/sensors.js');
@@ -26,6 +28,40 @@ router.route('/sensors')
         sensors.readSensorsData(forceRefresh, function (data) {
             res.json(data);
         });
+    });
+
+
+var aqdFileName = path.join(__dirname, '..', 'data', 'aq-detectors.json');
+
+router.route('/aq-detectors')
+    .get(function (req, res) {
+        var dataJson = fs.readFileSync(aqdFileName, 'utf8');
+        res.end(dataJson);
+    });
+
+router.route('/aq-detectors')
+    .post(function (req, res) {
+        var dataJson = fs.readFileSync(aqdFileName, 'utf8');
+        var data = JSON.parse(dataJson);
+        Object.assign(data, req.body);
+        fs.writeFileSync(aqdFileName, JSON.stringify(data, null, 2), 'utf8');
+        res.end(JSON.stringify(req.body));
+    });
+
+router.route('/aq-detectors/:id')
+    .get(function (req, res) {
+        var dataJson = fs.readFileSync(aqdFileName, 'utf8');
+        var data = JSON.parse(dataJson);
+        res.end(JSON.stringify(data[req.params.id], null, 2));
+    });
+
+router.route('/aq-detectors/:id')
+    .delete(function (req, res) {
+        var dataJson = fs.readFileSync(aqdFileName, 'utf8');
+        var data = JSON.parse(dataJson);
+        delete data[req.params.id];
+        fs.writeFileSync(aqdFileName, JSON.stringify(data, null, 2), 'utf8');
+        res.end();
     });
 
 module.exports = router;
